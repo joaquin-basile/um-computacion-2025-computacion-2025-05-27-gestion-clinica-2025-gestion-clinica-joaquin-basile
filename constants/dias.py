c452fe2bed9246c310242b261b49999a1242d8a5
joaquin-basile <j.basile@alumno.um.edu.ar>
2025-06-08 21:07:59 -0300

    Agregando el enum Dia para tener tipado seguro

---
+++ constants/dias.py
@@ -0,0 +1,10 @@
+from enum import Enum
+
+class Dia(Enum):
+    lunes = "lunes"
+    martes = "martes"
+    miercoles = "miércoles"
+    jueves = "jueves"
+    viernes = "viernes"
+    sabasto = "sábado"
+    domingo = "domingo"
